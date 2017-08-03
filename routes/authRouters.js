b1cf3f4d530a2b60cf75357e573197d6b729a68d
m-alchemist <alkhamis@email.arizona.edu>
2017-08-03 10:30:03 -0700

    final authentication fully ready

--- routes/authRouters.js
+++ routes/authRouters.js
@@ -1,10 +1,15 @@
 
 const passport=require('passport');
+app.get('/',(req,res)=>{
+  res.send('hello':'world')
+})
 module.exports=(app)=>{
   app.get('/auth/google',passport.authenticate('google',{
     scope:['profile','email']
   })
   );
+
+
 
   app.get('/auth/google/callback',passport.authenticate('google'));
   app.get('/api/current-user',(req,res)=>{
